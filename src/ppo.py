fixed logprob bug

src/ppo.py
@@ -14,35 +14,38 @@ LearnerState = collections.namedtuple("LearnerState", "count actor_opt_state cri
 ActorOutput = collections.namedtuple("ActorOutput", "action log_prob advantage")
 
 def build_actor_network(num_actions: int) -> hk.Transformed:
-  """Factory for a simple MLP network."""
   def network(obs):
-    network = hk.Sequential(
-        [
-         nets.MLP([64, 64, num_actions], activation=jnp.tanh),
-         ])
+    network = nets.MLP(
+      [64, 64, num_actions],
+      activation=jnp.tanh,
+      w_init=hk.initializers.Orthogonal()
+      )
+
     return network(obs)
   return hk.without_apply_rng(hk.transform(network, apply_rng=True))
 
-def build_critic_network(num_actions: int) -> hk.Transformed:
-  """Factory for a simple MLP network."""
+def build_critic_network() -> hk.Transformed:
   def network(obs):
     network = hk.Sequential(
-        [nets.MLP([64, 64, 1], activation=jnp.tanh)])
+        [nets.MLP([64, 64, 1],
+                  activation=jnp.tanh,
+                  w_init=hk.initializers.Orthogonal())
+         ])
     return network(obs)
   return hk.without_apply_rng(hk.transform(network, apply_rng=True))
 
 
 class PPO:
   """A simple PPO agent."""
-  def __init__(self, observation_spec, action_spec, learning_rate):
+  def __init__(self, observation_spec, action_spec, actor_learning_rate, critic_learning_rate):
     self._observation_spec = observation_spec
     self._action_spec = action_spec
 
     # Neural nets and optimisers.
     self._actor = build_actor_network(action_spec)
-    self._critic = build_critic_network(action_spec)
-    self._actor_optimizer = optax.sgd(0.001)
-    self._critic_optimizer = optax.sgd(0.01)
+    self._critic = build_critic_network()
+    self._actor_optimizer = optax.adam(actor_learning_rate)
+    self._critic_optimizer = optax.adam(critic_learning_rate)
 
     # Jitting for speed.
     self.actor_step = jax.jit(self.actor_step)
@@ -66,57 +69,61 @@ class PPO:
 
   def actor_step(self, params, env_output, actor_state, key, evaluation)\
           -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, ActorState]:
-    obs = jnp.expand_dims(env_output.observation, 0)  # dummy batch
-    logits = self._actor.apply(params.actor, obs)[0]
-    value = self._critic.apply(params.critic, obs)[0]
-    # sample from policy
-    train_a = jax.random.categorical(key, logits).squeeze()
-    # else be greedy
-    eval_a = jnp.argmax(logits)
-    action = jax.lax.select(evaluation, eval_a, train_a)
-    return value, action, logits[action], ActorState(actor_state.count + 1)
 
-  def learner_step(self, params, data, learner_state, unused_key) -> Tuple[jnp.ndarray, jnp.ndarray, Params, LearnerState]:
-    obs_tm1, a_t, logpi_tm1, advs_tm1, rtgs_t, discount_t, obs_t = data
-    advs_tm1 = (advs_tm1 - advs_tm1.mean()) / (advs_tm1.std() + 1e-8)
+    obs = jnp.expand_dims(env_output.observation, 0)  # dummy batch
+    pi = self._actor.apply(params.actor, obs)[0]
+    value = self._critic.apply(params.critic, obs)[0]
+
+    # sample from policy
+    train_a = jax.random.categorical(key, pi).squeeze()
+    # else be greedy
+    eval_a = jnp.argmax(pi)
+    action = jax.lax.select(evaluation, eval_a, train_a)
+    return value, action, pi, ActorState(actor_state.count + 1)
+
+  def learner_step(self, params, data, learner_state, unused_key)\
+          -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, Params, LearnerState]:
+
+    obs_tm1, a_t, pi_t, adv_t, rtgs_t, discount_t, obs_t = data
 
     # update critic
-    critic_loss = self._critic_loss(params.critic, obs_tm1, rtgs_t)
-    dl_dc = jax.grad(self._critic_loss)(params.critic, obs_tm1, rtgs_t)
+    critic_loss, dl_dc = jax.value_and_grad(self._critic_loss)(params.critic, obs_tm1, rtgs_t)
     critic_updates, critic_opt_state = self._critic_optimizer.update(dl_dc, learner_state.critic_opt_state)
     critic_params = optax.apply_updates(params.critic, critic_updates)
 
-    actor_loss, entropy = self._ppo_loss(params.actor, learner_state.clip, obs_tm1, a_t, logpi_tm1, advs_tm1),\
-                          self._entropy_loss(params.actor, obs_t)
+    ppo_loss, entropy = self._ppo_loss(params.actor, learner_state.clip, obs_tm1, a_t, pi_t, adv_t),\
+                        self._entropy_loss(params.actor, obs_t)
 
     # update actor
     dl_da = jax.grad(self._actor_loss)(params.actor, learner_state.clip, learner_state.beta,
-                                       obs_tm1, a_t, logpi_tm1, advs_tm1, obs_t)
+                                       obs_tm1, a_t, pi_t, adv_t, obs_t)
     actor_updates, actor_opt_state = self._actor_optimizer.update(dl_da, learner_state.actor_opt_state)
     actor_params = optax.apply_updates(params.actor, actor_updates)
 
     return (
-      actor_loss, critic_loss, entropy,
+      ppo_loss, critic_loss, entropy,
       Params(actor_params, critic_params),
-      LearnerState(learner_state.count + 1, actor_opt_state, critic_opt_state, learner_state.clip, learner_state.beta))
-
-  def _actor_loss(self, actor_params, clip, beta, obs_tm1, a_tm1, logpi_tm1, advs, obs_t):
-    return self._ppo_loss(actor_params, clip, obs_tm1, a_tm1, logpi_tm1, advs) \
-           + beta * self._entropy_loss(actor_params, obs_t)
-
+      LearnerState(learner_state.count + 1, actor_opt_state, critic_opt_state, learner_state.clip, learner_state.beta)
+    )
   def _critic_loss(self, critic_params, obs_tm1, target):
+    # target policy is rewards-to-go
     q_tm1 = self._critic.apply(critic_params, obs_tm1)
     return jnp.power(jax.lax.stop_gradient(target) - q_tm1, 2).mean()
 
-  def _ppo_loss(self, actor_params, eps, obs_tm1, a_tm1, logpi_tm1, advs):
-    logits_t = self._actor.apply(actor_params, obs_tm1)
-    logpi_t = softmax().logprob(a_tm1, logits_t)
+  def _actor_loss(self, actor_params, clip, beta, obs_tm1, a_tm1, pi_t, advs, obs_t):
+    return self._ppo_loss(actor_params, clip, obs_tm1, a_tm1, pi_t, advs) \
+           + beta * self._entropy_loss(actor_params, obs_t)
 
-    ratio = jnp.exp(logpi_t - logpi_tm1)
-    surr1 = ratio * advs
-    surr2 = jnp.clip(ratio, 1.0 - eps, 1.0 + eps) * advs
+  def _ppo_loss(self, actor_params, eps, obs_tm1, a_tm1, pi_t, adv):
+    logits_t = self._actor.apply(actor_params, obs_tm1)
+    logpi_new = softmax().logprob(a_tm1, logits_t)
+    logpi_old = softmax().logprob(a_tm1, pi_t)
+
+    ratio = jnp.exp(logpi_new - logpi_old)
+    surr1 = ratio * adv
+    surr2 = jnp.clip(ratio, 1.0 - eps, 1.0 + eps) * adv
     return -1 * jnp.minimum(surr1, surr2).mean()
 
   def _entropy_loss(self, actor_params, obs_t):
-    logits_t = self._actor.apply(actor_params, obs_t)
-    return -1 * softmax().entropy(logits_t).mean()
+    pi_t = self._actor.apply(actor_params, obs_t)
+    return -1 * softmax().entropy(pi_t).mean()

src/replay_buffer.py
@@ -1,4 +1,4 @@
-from typing import Optional, NamedTuple
+from typing import Optional, NamedTuple, Tuple
 
 import jax.numpy as jnp
 import random
@@ -13,19 +13,19 @@ class EnvOutput(NamedTuple):
   info: Optional[str] = None
 
 class Trajectory(NamedTuple):
-  """A trajectory is a sequence of observations, actions, rewards, discounts.
+  """A trajectory is a sequence of observations, ... etc.
   Note: `observations` should be of length T+1 to make up the final transition.
   """
   observations: jnp.ndarray  # [T + 1, ...]
   actions: jnp.ndarray  # [T]
-  logpi: jnp.ndarray  # [T]
+  pi: jnp.ndarray  # [T]
   gae: jnp.ndarray  # [T]
   rtg: jnp.ndarray  # [T]
   rewards: jnp.ndarray  # [T]
   discounts: jnp.ndarray  # [T]
 
 class SequenceBuffer:
-  """A simple buffer for accumulating trajectories."""
+  """A buffer for accumulating trajectories."""
   _observations: jnp.ndarray
   _actions: jnp.ndarray
   _rewards: jnp.ndarray
@@ -38,27 +38,28 @@ class SequenceBuffer:
   def __init__(
       self,
       obs_spec: jnp.array,
-      max_sequence_length: int,
+      action_spec: jnp.array,
+      max_length: int,
   ):
     """Pre-allocates buffers of numpy arrays to hold the sequences."""
     self._observations = np.zeros(
-        shape=(max_sequence_length + 1, obs_spec))
+        shape=(max_length + 1, obs_spec))
     self._values = np.zeros(
-        shape=max_sequence_length, dtype=jnp.float32)
+        shape=max_length, dtype=jnp.float32)
     self._actions = np.zeros(
-        shape=max_sequence_length, dtype=jnp.float32)
-    self._logpi = np.zeros(
-        shape=max_sequence_length, dtype=jnp.float32)
-    self._rewards = np.zeros(max_sequence_length, dtype=jnp.float32)
-    self._discounts = np.zeros(max_sequence_length, dtype=jnp.float32)
-    self._max_sequence_length = max_sequence_length
+        shape=max_length, dtype=jnp.float32)
+    self._pi = np.zeros(
+        shape=(max_length, action_spec))
+    self._rewards = np.zeros(max_length, dtype=jnp.float32)
+    self._discounts = np.zeros(max_length, dtype=jnp.float32)
+    self._max_sequence_length = max_length
 
   def append(
       self,
       timestep: EnvOutput,
       value: int,
       action: int,
-      log_pi: int,
+      pi: int,
   ):
     """Appends an observation, action, reward, and discount to the buffer."""
     if self.full():
@@ -70,11 +71,11 @@ class SequenceBuffer:
       self._observations[self._t] = timestep.observation
       self._needs_reset = False
 
-    # Append (o, a, r, d) to the sequence buffer.
+    # Append (o, v, a, pi, r, d) to the sequence buffer.
     self._observations[self._t + 1] = timestep.observation
     self._values[self._t] = value
     self._actions[self._t] = action
-    self._logpi[self._t] = log_pi
+    self._pi[self._t] = pi
     self._rewards[self._t] = timestep.reward
     self._discounts[self._t] = timestep.discount
     self._t += 1
@@ -101,13 +102,13 @@ class SequenceBuffer:
       gae[t] = delta + gamma * lmbda * (gae[t + 1] if t + 1 < self._t else 0)
 
     trajectory = Trajectory(
-        self._observations[:self._t + 1],
-        self._actions[:self._t],
-        self._logpi[:self._t],
-        gae,
-        rtg,
-        self._rewards[:self._t],
-        self._discounts[:self._t],
+      self._observations[:self._t + 1],
+      self._actions[:self._t],
+      self._pi[:self._t],
+      gae,
+      rtg,
+      self._rewards[:self._t],
+      self._discounts[:self._t],
     )
     self._t = 0  # Mark sequences as consumed.
     self._needs_reset = True
@@ -121,51 +122,46 @@ class SequenceBuffer:
     """Returns whether or not the trajectory buffer is full."""
     return self._t == self._max_sequence_length
 
-
 class ReplayBuffer(object):
-  """A simple replay buffer."""
-
+  """A buffer to store trajectories and apply normalisation"""
   def __init__(self, capacity):
-    self._prev = None
-    self._action = None
-    self._logpi = None
-    self._adv = None
-    self._latest = None
+    self._sum = 0
+    self._squared_sum = 0
     self.buffer = collections.deque(maxlen=capacity)
     self.capacity = capacity
 
-  def push(self, env_output, action, logpi):
-    self._prev = self._latest
-    self._action = action
-    self._logpi = logpi
-    self._latest = env_output
-
-    if action is not None:
+  def add_trajectory(self, traj: Trajectory) -> None:
+    for t in range(1, len(traj.actions)):
+      # add to buffer
       self.buffer.append(
-          (self._prev.observation, self._action, self._logpi, self._latest.reward,
-           self._latest.discount, self._latest.observation))
+        (traj.observations[t - 1], traj.actions[t], traj.pi[t], traj.gae[t],
+         traj.rtg[t], traj.discounts[t], traj.observations[t])
+      )
+      # for normalisation stats
+      self._sum += traj.gae[t]
+      self._squared_sum += traj.gae[t] * traj.gae[t]
 
-  def sample(self, batch_size):
-    obs_tm1, a_tm1, logpi_t, adv_t, rtg_t, discount_t, obs_t = zip(
+  def sample(self, batch_size: int) -> Tuple:
+    obs_tm1, a_tm1, pi_tm1, adv_t, rtg_t, discount_t, obs_t = zip(
         *random.sample(self.buffer, batch_size))
 
-    return (jnp.stack(obs_tm1), jnp.asarray(a_tm1), jnp.asarray(logpi_t), jnp.asarray(adv_t),
-            jnp.asarray(rtg_t), jnp.asarray(discount_t), jnp.stack(obs_t))
+    return (jnp.stack(obs_tm1),
+            jnp.asarray(a_tm1),
+            jnp.stack(pi_tm1),
+            self._normalize(jnp.asarray(adv_t)),
+            jnp.asarray(rtg_t),
+            jnp.asarray(discount_t),
+            jnp.stack(obs_t))
 
-  def is_ready(self, batch_size):
+  def _normalize(self, values: jnp.ndarray) -> jnp.ndarray:
+    _mean = self._sum/len(self.buffer)
+    _std = np.sqrt(self._squared_sum / len(self.buffer) - _mean ** 2)
+    return (values - _mean) / (_std + 1e-8)
+
+  def is_ready(self, batch_size) -> bool:
     return batch_size <= len(self.buffer)
 
-  def add_trajectory(self, traj: Trajectory):
-    for t in range(1, len(traj.actions)):
-      self.buffer.append(
-        (traj.observations[t-1], traj.actions[t], traj.logpi[t], traj.gae[t],
-          traj.rtg[t], traj.discounts[t], traj.observations[t])
-      )
-
   def reset(self):
-    self._prev = None
-    self._action = None
-    self._logpi = None
-    self._adv = None
-    self._latest = None
+    self._sum = 0
+    self._squared_sum = 0
     self.buffer = collections.deque(maxlen=self.capacity)

src/test_replay_buffer.py
@@ -2,7 +2,7 @@ from replay_buffer import SequenceBuffer, EnvOutput
 import jax.numpy as jnp
 
 def test_sequence_buffer():
-    buffer = SequenceBuffer(obs_spec=1, max_sequence_length=5)
+    buffer = SequenceBuffer(obs_spec=1, max_length=5)
 
     timestamp = EnvOutput(observation=jnp.ones(1),
                          reward=jnp.ones(1),
@@ -10,8 +10,9 @@ def test_sequence_buffer():
                          info=None)
 
     for i in range(5):
-        buffer.append(timestamp, value=1, action=0, log_pi=0)
+        buffer.append(timestamp, value=1, action=0, pi=0)
 
     result_trajectory = buffer.drain(gamma=0.5, lmbda=1)
 
     assert (result_trajectory.rtg == jnp.array([4, 3, 2, 1, 0])).all()
+    assert (result_trajectory.gae == jnp.array([1,2,3,4,5])).all()